0f4db535053b057109deeaba98502c0904ea67ff
Fizur-Rahman-Fahim <fizurrahman733433@gmail.com>
2025-05-11 15:02:53 +0600

    Postgray SQL added

--- library_management/settings.py
+++ library_management/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
     )
 }
 
